781157de9f6af4da4bac239067fd3993a6ec52e3
Marc Rodriguez <marc.herman.rodriguez@gmail.com>
2021-04-06 12:00:16 -0700

    completed

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-
+function superbowlWin(arr) {
+    const win = arr.find(
+    function find(element, index, array) {
+        return (element.result === 'W');
+    })
+    if (win !== undefined) {
+        return win.year;
+    }
+}
