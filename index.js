dc31d1f4dd965df9fc1194d35bf7ea45b9f7ecf5
Marc Rodriguez <marc.herman.rodriguez@gmail.com>
2021-04-06 15:53:22 -0700

    cleaned up a bit

--- index.js
+++ index.js
@@ -1,8 +1,5 @@
 function superbowlWin(arr) {
-    const win = arr.find(
-    function find(element, index, array) {
-        return (element.result === 'W');
-    })
+    const win = arr.find(element => (element.result === 'W'))
     if (win !== undefined) {
         return win.year;
     }
